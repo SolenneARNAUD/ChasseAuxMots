4cb64d421f863d172a9ceec85c6376735a406766
Vincent Kapfer <vincentkapf@gmail.com>
2025-12-09 15:36:44 +0100

    création de la classe symbole

---
+++ Symbole.py
@@ -0,0 +1,55 @@
+from ctypes.wintypes import COLORREF
+import string
+
+
+class Symbole(object):
+    """
+    Cette classe a pour objectif d'encoder les lettres/symboles
+    présent dans les mots. Les lettres/symboles doivent être 
+    indépendant car elles sont tapé une par une
+    """
+    
+    def __init__(self, couleur, symbole):
+        """
+        Constructeur :
+        Initialise les attributs de l'objet.
+        """
+        self.state = True
+        self.couleur = couleur
+        self.symbole = symbole
+    
+
+# ---------- GETTERS / SETTERS ----------
+    @property
+    def state(self):
+        return self.state
+
+    @state.setter
+    def state(self, value):
+        if (value is bool):
+            self.state = value
+            
+    @property
+    def couleur(self):
+        return self.couleur
+
+    @couleur.setter
+    def couleur(self, value):
+        if (value is COLORREF):
+            self.couleur = value
+    
+    @property
+    def symbole(self):
+        return self.symbole
+
+    @symbole.setter
+    def symbole(self, value):
+        if (value is str):
+            self.symbole = value
+
+
+
+
+
+
+
